04da754af751c48c4b579ac27e29b65812c2ff7c
matiasgluck <matiasugluck@gmail.com>
2025-05-09 18:56:43 -0300

    feat: add custom bottom bar buttons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 import {Tabs} from 'expo-router';
 
 import Colors from '@/constants/Colors';
 import {useColorScheme} from '@/components/useColorScheme';
 import {useClientOnlyValue} from '@/components/useClientOnlyValue';
 import {Navbar} from "@/components/Navbar";
-
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{marginBottom: -3}} {...props} />;
-}
+import {BottomBarButton} from "@/components/BottomBarButton";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -25,12 +17,13 @@ export default function TabLayout() {
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
+        tabBarStyle: {height: 80},
       }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Reminders',
-          tabBarIcon: ({color}) => <TabBarIcon name="bell" color={color}/>,
+          tabBarButton: (props) => <BottomBarButton props={props} icon="bell" label="Reminders"/>,
           header: (props) => <Navbar bottomHeaderTabProps={props}/>,
         }}
       />
@@ -38,7 +31,7 @@ export default function TabLayout() {
         name="important-reminders"
         options={{
           title: 'Favorites',
-          tabBarIcon: ({color}) => <TabBarIcon name="star" color={color}/>,
+          tabBarButton: (props) => <BottomBarButton props={props} icon="star" label="Favorites"/>,
           header: (props) => <Navbar bottomHeaderTabProps={props}/>,
         }}
       />
@@ -46,7 +39,7 @@ export default function TabLayout() {
         name="create-reminder"
         options={{
           title: 'Create Reminder',
-          tabBarIcon: ({color}) => <TabBarIcon name="plus-circle" color={color}/>,
+          tabBarButton: (props) => <BottomBarButton props={props} icon="plus-circle" label="Create Reminder"/>,
           header: (props) => <Navbar bottomHeaderTabProps={props} headerTitle="Add a Reminder"/>,
         }}
       />
---
+++ components/BottomBarButton.tsx
@@ -0,0 +1,69 @@
+import {StyleSheet, TouchableOpacity} from "react-native";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {BottomTabBarButtonProps} from "@react-navigation/bottom-tabs";
+import {useTheme} from "@/hooks/useTheme";
+import {useEffect} from "react";
+import {usePathname} from "expo-router";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
+
+interface Props {
+  props: BottomTabBarButtonProps;
+  icon: string;
+  label?: string;
+}
+
+export const BottomBarButton = ({props, icon, label = ""}: Props) => {
+  const {theme} = useTheme();
+  const pathname = usePathname();
+
+  const isFocused = pathname === props.href;
+
+  const scale = useSharedValue(isFocused ? 1.2 : 1);
+
+  useEffect(() => {
+    scale.value = withTiming(isFocused ? 1.2 : 1, {duration: 200});
+  }, [isFocused]);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{scale: scale.value}],
+  }));
+
+  return (
+    <TouchableOpacity
+      activeOpacity={0.7}
+      style={[styles.container, {backgroundColor: theme.accent}]}
+      onPress={props.onPress}
+    >
+      <Animated.View style={animatedStyle}>
+        {/* @ts-ignore */}
+        <FontAwesome name={icon} size={24} color={theme.foregroundSecondary}/>
+      </Animated.View>
+      <Animated.Text
+        style={[
+          styles.label,
+          {color: theme.foregroundSecondary},
+          animatedStyle,
+        ]}
+      >
+        {label}
+      </Animated.Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  label: {
+    marginTop: 4,
+    fontSize: 14,
+  },
+});
