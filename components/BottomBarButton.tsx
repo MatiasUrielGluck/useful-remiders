f88d580f4697e5f2fc4e6f29294600bc263ed381
matiasgluck <matiasugluck@gmail.com>
2025-05-09 19:08:29 -0300

    feat: add more animations to the bottom bar button

--- components/BottomBarButton.tsx
+++ components/BottomBarButton.tsx
@@ -19,17 +19,19 @@ interface Props {
 export const BottomBarButton = ({props, icon, label = ""}: Props) => {
   const {theme} = useTheme();
   const pathname = usePathname();
-
   const isFocused = pathname === props.href;
 
   const scale = useSharedValue(isFocused ? 1.2 : 1);
+  const opacity = useSharedValue(isFocused ? 1 : 0.7);
 
   useEffect(() => {
-    scale.value = withTiming(isFocused ? 1.2 : 1, {duration: 200});
+    scale.value = withTiming(isFocused ? 1.2 : 1, {duration: 150});
+    opacity.value = withTiming(isFocused ? 1 : 0.7, {duration: 150});
   }, [isFocused]);
 
   const animatedStyle = useAnimatedStyle(() => ({
     transform: [{scale: scale.value}],
+    opacity: opacity.value,
   }));
 
   return (
